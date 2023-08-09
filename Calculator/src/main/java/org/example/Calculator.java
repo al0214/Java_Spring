bb028e9e9e254f9e59f9240abe086fba1bbd1f78
al0214 <sisang951@hansei.sen.hs.kr>
2023-08-09 23:57:28 +0900

    Add Credit Calculator

---
+++ Calculator/src/main/java/org/example/Calculator.java
@@ -0,0 +1,22 @@
+package org.example;
+
+
+import org.example.calculate.*;
+
+import java.util.List;
+
+
+
+public class Calculator {
+    private static final List<NewArithmeticOperator> arithmeticOperators = List.of(
+            new AdditionOperator(),
+            new SubtractionOperator(),
+            new MultiplicationOperator(),
+            new DivisionOperator());
+    public static int arithmeticCalculate(PositiveNumber operand1, String operator, PositiveNumber operand2){
+        return arithmeticOperators.stream().filter(arithmeticOperators -> arithmeticOperators.supports(operator))
+                .map(arithmeticOperators -> arithmeticOperators.calculator(operand1,operand2))
+                .findFirst()
+                .orElseThrow(()->new IllegalArgumentException("올바른 사칙 연산이 아닙니다."));
+    }
+}
